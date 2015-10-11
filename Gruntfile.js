ac401dfaf95190589510318b44cedd01b2811698
rahul <rahul4her@hotmail.com>
2015-10-12 01:39:50 +0530

    dry run

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,6 +119,7 @@ module.exports = function (grunt) {
     }
   });
 
+
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-connect');
