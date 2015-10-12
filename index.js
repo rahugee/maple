68f27cc267abbd85f5de7b989a6d3800012af539
rahul <rahul4her@hotmail.com>
2015-10-12 18:10:11 +0530

    Index updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var file = new static.Server({
     cache: 86400,
     serverInfo: "maple-cdn-server",
     indexFile: "index.html",
+    gzip: true,
     headers : {
         "Cache-Control" : "max-age=86400, public",
         'Access-Control-Allow-Origin' : "*",
@@ -12,17 +13,26 @@ var file = new static.Server({
 });
 require('http').createServer(function(request, response) {
     try{
-        if((/\/(src|dist|build)\//).test(request.url)){
-            request.addListener('end', function() {
-                file.serve(request, response);
-            }).resume();
+        console.log("File: ",request.url);
+        if((/\/(src|dist)\//).test(request.url)){
+            file.serve(request, response,function (err, result) {
+                if (err) { // There was an error serving the file
+                    console.error("Error serving " + request.url + " - " + err.message);
+                    // Respond to the client
+                    response.writeHead(err.status, err.headers);
+                    response.write("NF:"+request.url);
+                    response.end();
+                }
+            });
         } else {
-            request.addListener('end', function() {
-                file.serveFile("index.html",200, {}, request, response);
-            }).resume();
+            //file.serveFile("index.html",200, {}, request, response);
+            response.write("NF:"+request.url);
+            response.end();
         }
     } catch(e){
-        response.write("Error request.url",request.url);
+        //response.write("Error request.url",request.url);
+        //response.end();
+        response.write("NF:"+request.url);
         response.end();
     }
 
